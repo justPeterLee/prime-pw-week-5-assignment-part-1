4ea759d82a022d8eda295c5e602e7da148212a64
Peter Lee <PeterLee2094@gmail.com>
2022-10-26 15:36:57 -0500

    finished question 4

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -63,6 +63,15 @@ console.log(me.favThreeFoods[me.favThreeFoods.length - 1]);
   - Set your object's shoeCount to be what it was plus 1
   - Console.log your updated shoe count. 
 */
+console.log(me.shoeCount);
+me.shoeCount += 1;
+console.log(me.shoeCount);
+
+
+
+
+
+
 
 
 /* 5. Add a new property to an existing object.
